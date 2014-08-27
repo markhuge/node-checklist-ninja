c67b205c9937f70db34dce82fdad2850ba797dae
Mark Wilkerson <mark.wilkerson@hightail.com>
2014-08-26 18:44:34 -0700

    formatting

--- test/mocks.js
+++ test/mocks.js
@@ -1,21 +1,21 @@
 this.raw = function(method, endpoint, payload, callback) {
-        if (arguments.length === 3) {
-          callback = payload;
-        }
-        callback(null, 200, {method: method, endpoint: endpoint, payload: payload });
-      };
+    if (arguments.length === 3) {
+      callback = payload;
+    }
+    callback(null, 200, {method: method, endpoint: endpoint, payload: payload });
+  };
 
 
 this.put = function (endpoint, payload, callback) {
-      if (payload.title === 'failcode' || payload.display_text === 'failcode' || payload.label === 'failcode') return callback(undefined, 200, null);
-      return callback(undefined, 201, {endpoint: endpoint, payload: payload });
-    };
+    if (payload.title === 'failcode' || payload.display_text === 'failcode' || payload.label === 'failcode') return callback(undefined, 200, null);
+    return callback(undefined, 201, {endpoint: endpoint, payload: payload });
+  };
 
 this.get = function (endpoint, callback) {
-      if (endpoint === '/failerr')  return callback("Error msg", 200, null);
-      if (endpoint === '/failcode') return callback(undefined, 404, "Not found");
-      return callback(undefined, 200, endpoint);
-    };
+    if (endpoint === '/failerr')  return callback("Error msg", 200, null);
+    if (endpoint === '/failcode') return callback(undefined, 404, "Not found");
+    return callback(undefined, 200, endpoint);
+  };
 
 this.request = function (options, callback) {
   if (options.err === true) return callback("Error as expected");
